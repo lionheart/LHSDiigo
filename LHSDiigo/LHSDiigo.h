7b15247e01e5d6178149acdaba82529fe74877cd
Dan Loewenherz <dloewenherz@gmail.com>
2014-03-25 17:05:58 -0500

    initial commit

---
+++ LHSDiigo/LHSDiigo.h
@@ -0,0 +1,13 @@
+//
+//  LHSDiigo.h
+//  LHSDiigo
+//
+//  Created by Dan Loewenherz on 3/25/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LHSDiigo : NSObject
+
+@end
---
+++ LHSDiigoTests/LHSDiigoTests.m
@@ -0,0 +1,34 @@
+//
+//  LHSDiigoTests.m
+//  LHSDiigoTests
+//
+//  Created by Dan Loewenherz on 3/25/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LHSDiigoTests : XCTestCase
+
+@end
+
+@implementation LHSDiigoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
